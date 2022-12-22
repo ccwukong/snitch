56393cc049d7f41767c128c43581cd4ab219b6e5
ccwukong <ccwukong@gmail.com>
2022-12-22 15:06:36 +0800

    minor fix

--- src/snitch/main.py
+++ src/snitch/main.py
@@ -22,32 +22,34 @@ async def run(path, output):
             if config.has_postman_collection:
                 pp = PostmanFileParser(
                     config.collection_file_path, config.metadata)
-        res = await run_health_check(pp.requests)
 
-        if output and os.path.exists(output):
-            f = open(os.path.join(
-                output, f'hc_{time()}_{uuid.uuid4()}.txt'), 'w')
-            s = f"Total endpoints checked: {res['total']}\nSuccess: {res['success']}\nErrors: {res['errors']}\n\n\n"
-            for r in res['responses']:
-                s += f"{r}\n{'-'*50}\n"
-            f.write(s)
-            f.close()
-        else:
-            click.echo(click.style(
-                'Error: output destination directory doesn\'t exist.', fg='red'))
+                res = await run_health_check(pp.requests)
 
-        click.echo(click.style('Health check', fg='green'))
-        click.echo('-'*50)
-        for r in res['responses']:
-            click.echo(r)
-            click.echo('-'*50)
+                if output and os.path.exists(output):
+                    f = open(os.path.join(
+                        output, f'hc_{time()}_{uuid.uuid4()}.txt'), 'w')
+                    s = f"Total endpoints checked: {res['total']}\nSuccess: {res['success']}\nErrors: {res['errors']}\n\n\n"
+                    for r in res['responses']:
+                        s += f"{r}\n{'-'*50}\n"
+                    f.write(s)
+                    f.close()
+                else:
+                    click.echo(click.style(
+                        'Error: output destination directory doesn\'t exist.', fg='red'))
 
-        click.echo(click.style(
-            f"Total endpoints checked: {res['total']}", fg='green'))
-        click.echo(click.style(f"Success: {res['success']}", fg='green'))
-        click.echo(click.style(f"Errors: {res['errors']}", fg='red'))
-        click.echo(click.style(
-            f"Success rate: {(res['success']/(res['success']+res['errors'])):.2f}%", fg='green'))
+                click.echo(click.style('Health check', fg='green'))
+                click.echo('-'*50)
+                for r in res['responses']:
+                    click.echo(r)
+                    click.echo('-'*50)
+
+                click.echo(click.style(
+                    f"Total endpoints checked: {res['total']}", fg='green'))
+                click.echo(click.style(
+                    f"Success: {res['success']}", fg='green'))
+                click.echo(click.style(f"Errors: {res['errors']}", fg='red'))
+                click.echo(click.style(
+                    f"Success rate: {(res['success']/(res['success']+res['errors'])):.2f}%", fg='green'))
     except Exception as e:
         click.echo(e)
 
